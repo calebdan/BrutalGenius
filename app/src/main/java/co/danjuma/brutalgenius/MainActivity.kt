eb5309af5f9183379e4b9b1e04c3ed40aff6d7e2
Caleb Tafawa-balewa <tafawabalewacaleb@gmail.com>
2023-11-23 18:48:47 +0000

    font addition

--- app/src/main/java/co/danjuma/brutalgenius/MainActivity.kt
+++ app/src/main/java/co/danjuma/brutalgenius/MainActivity.kt
@@ -3,7 +3,6 @@ package co.danjuma.brutalgenius
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.ui.text.googlefonts.GoogleFont
 import co.danjuma.brutalgenius.app.BrutalGeniusApp
 
 class MainActivity : ComponentActivity() {
