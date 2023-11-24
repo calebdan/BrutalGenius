dd51e89998b20ca61d9dff7ae404a9bd1c7187ee
Caleb Tafawa-balewa <tafawabalewacaleb@gmail.com>
2023-11-24 20:17:03 +0000

    Made some improvement on the UI and also added an external font.

--- app/src/main/java/co/danjuma/brutalgenius/app/BrutalGenius.kt
+++ app/src/main/java/co/danjuma/brutalgenius/app/BrutalGenius.kt
@@ -1,8 +1,12 @@
 package co.danjuma.brutalgenius.app
 
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -16,13 +20,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import co.danjuma.brutalgenius.R
 import co.danjuma.brutalgenius.components.HeaderText
 import co.danjuma.brutalgenius.components.TopSectionApp
 import co.danjuma.brutalgenius.ui.theme.BGYellow
@@ -84,13 +91,17 @@ fun BrutalGeniusApp() {
 
                 }
 
+                Spacer(modifier = Modifier.height(30.dp))
+
+                Text(
+                    text = "How to Play?", fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
             }
-
         }
-
-
     }
 }
+
 
 @Preview
 @Composable
